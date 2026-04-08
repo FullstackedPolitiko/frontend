f6b30c01ce5e9553fbbbba35255332b7dced5cc3
JalokinBo <144294697+JalokinBo@users.noreply.github.com>
2026-04-08 14:47:59 +0200

    MockTest til Malde

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,10 @@
 import "@testing-library/jest-dom";
+import {afterEach} from 'vitest';
+import {cleanup} from '@testing-library/react';
+import '@testing-library/jest-dom/vitest';
+import '@testing-library/jest-dom';
+
+// Runs a cleanup after each test case
+afterEach(() => {
+    cleanup();
+});
