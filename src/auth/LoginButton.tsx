5abe1c506ab3fb90f056df3d452bf2bdbec64a7c
JalokinBo <144294697+JalokinBo@users.noreply.github.com>
2026-05-06 04:48:43 +0200

    Burde virke

--- src/auth/LoginButton.tsx
+++ src/auth/LoginButton.tsx
@@ -4,6 +4,7 @@ import { useAuth } from './authContext';
 
 export default function LoginButton() {
     const { login } = useAuth();
+// debugging    console.log('CLIENT_ID Vite is using:', import.meta.env.VITE_GOOGLE_CLIENT_ID);
 
     const handleSuccess = async (cred: CredentialResponse) => {
         if (!cred.credential) return;
